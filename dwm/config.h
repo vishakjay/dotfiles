7743edc86952e793f65178cd994bcd588db719c2
vishakjay <vishakjay@gmail.com>
2025-06-23 00:32:49 +0530

    adding new config file for dwm

--- dwm/config.h
+++ dwm/config.h
@@ -10,21 +10,31 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "InputMonoNerdFont:size=16" };
+static const char dmenufont[]       = "InputMonoNerdFont:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+char normbgcolor[]      = "#222222";
+char normbordercolor[]  = "#444444";
+char normfgcolor[]      = "#bbbbbb";
+char selbgcolor[]       = "#005577";
+char selbordercolor[]   = "#005577";
+char selfgcolor[]       = "#eeeeee";
+//char urgfgcolor[]       = "#ffffff";
+//char urgbgcolor[]       = "#ff0000";
+//char urgbordercolor[]   = "#ff0000";
+#include "/home/superman/.cache/wal/colors-wal-dwm.h"
+//static  char *colors[][3]      = {
+//	[SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+//	[SchemeSel]  = { selfgcolor, selbgcolor, selbordercolor  },
+//	[SchemeUrg] = { urgfgcolor, urgbgcolor, urgbordercolor },
+//};
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {"", "", "", "","", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -62,7 +72,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
